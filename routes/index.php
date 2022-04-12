e5b6b1734647cbdac0bf93597e6668bd0a5b99fd
Lucas Souza de Araujo <lucass.a.6991@gmail.com>
2022-04-12 17:10:59 -0300

    Atualizando para o Slim 4, criação da pasta routes e cofiguração do slim para mostrar 'Hello world!'.

---
+++ routes/index.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace routes;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
+
+?>
